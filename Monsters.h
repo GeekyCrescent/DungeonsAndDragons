b1d136ac30242ae89f00cfc527d2f191dc3ac684
adrian <a01665899@tec.mx>
2024-11-02 15:32:47 -0600

    Archivo .h de monstruos

--- Monsters.h
+++ Monsters.h
@@ -1,7 +1,38 @@
 #pragma once
+#include <iostream>
+using namespace std;
 
 class Monsters {
-
+private:
+    string name;
+    float cr;
+    string type;
+    string size;
+    int ac;
+    int hp;
+    string align;
+public:
+    Monsters(string name, float cr, string type, string size, int ac, int hp, string align) {
+        this->name = name;
+        this->cr = cr;
+        this->type = type;
+        this->size = size;
+        this->ac = ac;
+        this->hp = hp;
+        this->align = align;
+    }
+    string getName();
+    void setName(string name);
+    float getCR();
+    void setCR(float cr);
+    string getType();
+    void setType(string type);
+    string getSize();
+    void setSize(string size);
+    int getAC();
+    void setAC(int ac);
+    int getHP();
+    void setHP();
 };
 
 
